1c62dfa85809bb348e43bd4cb5d4801106e2318d
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-02-10 21:37:50 +0900

    feat : 커스텀 로그아웃

--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/users/controller/LoginController.java
@@ -1,5 +1,10 @@
 package io.security.springsecuritymaster.users.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,9 +16,18 @@ public class LoginController {
         return "login/login";
     }
 
-    @GetMapping("signup")
+    @GetMapping("/signup")
     public String signup() {
         return "login/signup";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login";
     }
 
 }
