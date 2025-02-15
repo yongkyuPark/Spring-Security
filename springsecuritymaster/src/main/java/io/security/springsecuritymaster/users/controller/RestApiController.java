acaf8b00b9e4a061aa8d733d6a1fce8e6b2117d8
Yongkyu Park <dydrbdjdsla@gmail.com>
2025-02-15 18:07:58 +0900

    feat : Rest 로그아웃 구현

--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/users/controller/RestApiController.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/users/controller/RestApiController.java
@@ -1,7 +1,12 @@
 package io.security.springsecuritymaster.users.controller;
 
 import io.security.springsecuritymaster.domain.dto.AccountDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +28,15 @@ public class RestApiController {
     @GetMapping("/admin")
     public AccountDto restAdmin(@AuthenticationPrincipal AccountDto accountDto) {
         return accountDto;
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "logout";
     }
 }
